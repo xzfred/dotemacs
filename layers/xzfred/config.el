50105a3d59d44dff5e0585f9b25772129891bf73
xzfred <xzfred@gmail.com>
2018-01-22 18:42:14 +0800

    test config

---
+++ layers/xzfred/config.el
@@ -0,0 +1,24 @@
+
+
+;; 自启动flymake
+;; (add-hook 'find-file-hooks 'flymake-find-file-hook)
+
+;;
+;;更改spacemacs内置设置
+;;
+;; (setq-default
+;;  dotspacemacs-startup-lists '((recents . 5)
+;;                               (projects . 7))
+;;  dotspacemacs-enable-lazy-installation 'unused
+;;  dotspacemacs-large-file-size 5
+;;  dotspacemacs-which-key-delay 0.3
+;;  dotspacemacs-maximized-at-startup t
+;;  dotspacemacs-line-numbers t
+;;  dotspacemacs-default-font '(
+;;                              ;; "Source Code Pro"
+;;                              "Menlo"
+;;                              :size 14
+;;                              :weight normal
+;;                              :width normal
+;;                              :powerline-scale 1.1)
+;;  )
---
+++ layers/xzfred/packages.el
@@ -0,0 +1,30 @@
+(defconst xzfred-packages
+          '()
+  "The list of Lisp packages required by the fidding layer.
+
+Each entry is either:
+          ")
+
+(setq xzfred-packages
+      '(
+        dracula-theme
+        )
+      )
+
+;; 配置包
+;; use-package 装载包
+;; :init 在包装载之前执行的代码
+;; :config 在包装载之后执行的代码
+;; :bind 自定义绑定包快捷键，例如:bind ("C-." . jumpweb-mode))
+;; :defer t 自动推迟加载，在空闲时间加载
+;; :disabled t 禁用
+
+;; dracula-theme主题
+(defun xzfred/init-dracula-theme ()
+  (use-package dracula-theme
+    :init
+    :config
+    (load-theme 'dracula t)
+    ))
+
+;; (setq <layer>-excluded-packages '(package1 package2 ...)
